f2a060c44b93b07eebf6e37d52542b42625a690a
Chris Powell <chris.i.powell@gmail.com>
2012-08-14 17:33:48 +0100

    Initial version

---
+++ checkurl.js
@@ -0,0 +1,30 @@
+var URLChecker = function(){
+	this.checker = new Worker('checkurl_worker.js');
+	
+	var scope = this;
+	document.addEventListener('__urlchecked', function(evt)
+	{
+		if(scope.oncheck) scope.oncheck(evt);
+	});
+	
+	this.checker.addEventListener('message', function(e)
+	{
+		console.debug(e.data);
+		var resp = JSON.parse(e.data);
+	
+		var evt = document.createEvent('Event');	
+		evt.initEvent('__urlchecked', true, true);
+		evt.url = resp.url;
+		evt.exists = resp.exists;
+		
+		document.dispatchEvent(evt);
+		
+	}, false);
+	
+
+	this.startCheck = function(url)
+	{
+		
+		this.checker.postMessage(url);
+	}
+};
